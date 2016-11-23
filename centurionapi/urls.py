b95d7010512362b44dab9155b22ab1f67034c281
Nirav Joshi <nirav.j05@gmail.com>
2016-11-23 21:19:40 +0530

    Install HTML Template for Angular UI

--- centurionapi/urls.py
+++ centurionapi/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-
+from django.views.generic import TemplateView
 from .api import datewisedetailknwerrcntsApi,datewiseerrcntsApi
 
 urlpatterns = [
     url(r'^dwdeknercnt$',datewisedetailknwerrcntsApi.as_view()),
-    url(r'^dwercnt$',datewiseerrcntsApi.as_view())
+    url(r'^dwercnt$',datewiseerrcntsApi.as_view()),
+    url(r'^home',TemplateView.as_view(template_name="centurionapi/Home.html")),
 ]
