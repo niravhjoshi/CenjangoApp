7b62411328f4acddcc3816508113f131206e6dac
Nirav Joshi <nirav.j05@gmail.com>
2016-11-23 14:48:34 +0530

    changes in my model and admin

--- centurionapi/models.py
+++ centurionapi/models.py
@@ -1,5 +1,28 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
+
+@python_2_unicode_compatible
+class datewisedetailknownerrcounts(models.Model):
+    cust_name = models.CharField(max_length=50)
+    month_name = models.CharField(max_length=50)
+    err_name = models.TextField()
+    appsrv_name = models.CharField(max_length=50)
+    date_date = models.DateField()
+    err_counts = models.IntegerField()
+
+    def __str__(self):
+        return "{}  {}  {}  {}  {}  {}".format(self.cust_name,self.month_name,self.err_name,self.appsrv_name,self.date_date,self.err_counts)
+
+@python_2_unicode_compatible
+class datewiseerrcounts(models.Model):
+    cust_name = models.CharField(max_length=50)
+    date_stamp = models.DateField()
+    month_name = models.CharField(max_length=50)
+    appsrv_name = models.CharField(max_length=50)
+    err_counts = models.IntegerField()
+
+    def __str__(self):
+        return "List {}".format(self.cust_name)
