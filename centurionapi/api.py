4aca68caaae5411d38c940c26e1e90285bd7a40f
Nirav Joshi <nirajosh@in.ibm.com>
2016-12-12 19:54:42 +0530

    Add filter based api call into rest framework
    This commit made for add filter to rest based api for only
    datewiseerrcntsApi

--- centurionapi/api.py
+++ centurionapi/api.py
@@ -12,3 +12,21 @@ class datewisedetailknwerrcntsApi(ListAPIView):
 class datewiseerrcntsApi(ListAPIView):
     queryset = datewiseerrcounts.objects.all()
     serializer_class = datewiseerrcntsSerializer
+
+    def get_queryset(self):
+        'allow filter to apply on api return result by customer name attribute'
+        queryset = datewiseerrcounts.objects.all()
+        cust_name = self.request.query_params.get('cust_name',None)
+        month_name = self.request.query_params.get('month_name',None)
+        appsrv_name = self.request.query_params.get('appsrv_name',None)
+        date_stamp = self.request.query_params.get('date_stamp',None)
+        if cust_name is not None:
+            queryset = queryset.filter(cust_name=cust_name)
+        if month_name is not None:
+            queryset = queryset.filter(month_name=month_name)
+        if appsrv_name is not None:
+            queryset = queryset.filter(appsrv_name=appsrv_name)
+        if date_stamp is not None:
+            queryset = queryset.filter(date_stamp=date_stamp)
+
+        return queryset
--- centurionapi/serializers.py
+++ centurionapi/serializers.py
@@ -9,7 +9,19 @@ class datewisedetailknownerrcntsSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class datewiseerrcntsSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = datewiseerrcounts
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(datewiseerrcntsSerializer, self).__init__(*args, **kwargs)
+        request = self.context.get("request")
+        if request and request.query_params.get('fields'):
+            fields = request.query_params.get('fields')
+            if fields:
+                fields = fields.split(',')
+                allowed = set(fields)
+                existing  = set(self.fields.keys())
+                for field_name in existing - allowed:
+                    self.fields.pop(field_name)
+
